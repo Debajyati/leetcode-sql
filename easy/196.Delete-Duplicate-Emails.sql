aad4a456b30662382f1cbdfe179dd6ab6d4e4af9
Debajyati <ddebajyati@gmail.com>
2025-02-12 19:30:57 +0530

    Solving easy problems DAY 1

---
+++ easy/196.Delete-Duplicate-Emails.sql
@@ -0,0 +1,8 @@
+-- Write your MySQL query statement below
+WITH MinIds AS (  
+    SELECT MIN(id) AS id  
+    FROM Person  
+    GROUP BY email  
+)  
+DELETE FROM Person  
+WHERE id NOT IN (SELECT id FROM MinIds);
